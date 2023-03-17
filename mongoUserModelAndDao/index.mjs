dc96037798ec76592ab4af983eb8b55cafe6db0b
javier-rosas <jr2dj@virginia.edu>
2023-03-17 10:32:19 -0400

    refactored code and daos implemented inside index.mjs

--- mongoUserModelAndDao/index.mjs
+++ mongoUserModelAndDao/index.mjs
@@ -1,45 +1,69 @@
 import middy from "middy";
 import { verifyTokenMiddleware } from "./auth/verifyTokenMiddleware.mjs";
-import { mongooseConnect } from "./mongoose/mongooseConnect.mjs"
-import UserDao from "./daos/UserDao.mjs"
+import { mongooseConnect } from "./mongoose/mongooseConnect.mjs";
+import UserDao from "./daos/UserDao.mjs";
 
 const jwtSecret = process.env.JWT_SECRET;
 
 /**
-The main handler function.
-* @param {Object} event - The event object.
-* @param {string} event.body - The user object.
-* @returns {Object} An object containing the status
-**/
-const myHandler = async (event, context) => {
+ * Creates a new user or updates an existing one in the database.
+ * @async
+ * @param {object} userJwtDecoded - Decoded JWT token for the user.
+ * @param {object} user - User data to create or update.
+ * @returns {Promise<object>} - The created or updated user object.
+ * @throws {Error} - If the email in the JWT token and the email in the user data don't match.
+ */
+const createUserOrUpdate = async (userJwtDecoded, user) => {
+  if (userJwtDecoded.email !== user.email) {
+    throw new Error("Decoded user email and user email in body don't match");
+  }
+
+  await mongooseConnect();
+  const userDao = new UserDao();
+  const newUser = await userDao.createOrUpdateUser(user);
+  return newUser;
+};
+
+/**
+ * Creates a response object with a given status code and body.
+ * @param {number} statusCode - The HTTP status code to return.
+ * @param {object} body - The response body.
+ * @returns {object} - The response object.
+ */
+const createResponse = (statusCode, body) => ({
+  statusCode,
+  body: JSON.stringify(body),
+});
+
+/**
+ * The main Lambda function handler.
+ * @async
+ * @param {object} event - The Lambda event object.
+ * @param {object} context - The Lambda context object.
+ * @returns {Promise<object>} - The response object.
+ */
+const mainHandler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   try {
     const userJwtDecoded = event.user;
     const user = JSON.parse(event.body);
-
-    if (userJwtDecoded.email !== user.email) {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ error: "Decoded user email and user email in body don't match" })
-      }
-    }
-    await mongooseConnect()
-    const userDao = new UserDao();
-    const newUser = await userDao.createOrUpdateUser(user)
-    return {
-      statusCode: 200,
-      body: JSON.stringify(user),
-    };
-  } catch(error) {
+    const newUser = await createUserOrUpdate(userJwtDecoded, user);
+    return createResponse(200, newUser);
+  } catch (error) {
     console.log("error", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Internal server error" }),
-    };
+    const errorMessage = error.message || "Internal server error";
+    return createResponse(500, { error: errorMessage });
   }
 };
 
-// export handler with middleware
-export const handler = middy(myHandler).use(
+/**
+ * A middleware-wrapped AWS Lambda handler function that uses a JWT token for authentication.
+ *
+ * @constant
+ * @type {function}
+ * @param {function} myHandler - The AWS Lambda handler function to be wrapped with middleware.
+ * @returns {function} - A middleware-wrapped AWS Lambda handler function that uses a JWT token for authentication.
+ */
+export const handler = middy(mainHandler).use(
   verifyTokenMiddleware({ secret: jwtSecret })
 );
--- processAudioFile/index.mjs
+++ processAudioFile/index.mjs
@@ -1,67 +1,140 @@
 import middy from "middy";
 import { verifyTokenMiddleware } from "./functionality/auth/verifyTokenMiddleware.mjs";
 import { transcribeAudioFile } from "./functionality/main/transcribeAudioFile.mjs";
-import { mongooseConnect } from "./functionality/mongoose/mongooseConnect.mjs"
-import TranscriptDao from "./functionality/daos/TranscriptDao.mjs"
+import { mongooseConnect } from "./functionality/mongoose/mongooseConnect.mjs";
+import TranscriptDao from "./functionality/daos/TranscriptDao.mjs";
 import DiarizedTranscriptDao from "./functionality/daos/DiarizedTranscriptDao.mjs";
 
 const bucketName = process.env.BUCKET_NAME;
 const jwtSecret = process.env.JWT_SECRET;
 
 /**
-The main handler function.
-* @param {Object} event - The event object.
-* @param {Object} event.user - The user object from the JWT token.
-* @param {string} event.body - The request body as a JSON string.
-* @returns {Object} An object containing the status
-**/
+ * Create a transcript object from an audio file object and a user object.
+ *
+ * @async
+ * @function
+ * @param {Object} audioFileObj - An object containing information about an audio file.
+ * @param {string} audioFileObj.filename - The name of the audio file.
+ * @param {string} audioFileObj.language - The language of the audio file.
+ * @param {boolean} audioFileObj.enableSpeakerDiarization - Whether to enable speaker diarization.
+ * @param {number} audioFileObj.minSpeakerCount - The minimum number of speakers to diarize.
+ * @param {number} audioFileObj.maxSpeakerCount - The maximum number of speakers to diarize.
+ * @param {string} audioFileObj.dateAdded - The date the audio file was added.
+ * @param {string} audioFileObj.description - A description of the audio file.
+ * @param {Object} user - An object representing the user.
+ * @param {string} user.email - The email address of the user.
+ * @returns {Promise<Object>} - A Promise that resolves with the transcript object.
+ */
+const createTranscript = async (audioFileObj, user) => {
+  const {
+    filename,
+    language,
+    enableSpeakerDiarization,
+    minSpeakerCount,
+    maxSpeakerCount,
+    dateAdded,
+    description,
+  } = audioFileObj;
+
+  const languageCode = language === "Spanish" ? "es-ES" : "en-US";
+  const bucketPath = `gs://${bucketName}/${user.email}/${filename}`;
+  await mongooseConnect();
+  const transcript = await transcribeAudioFile(
+    bucketPath,
+    languageCode,
+    enableSpeakerDiarization,
+    minSpeakerCount,
+    maxSpeakerCount
+  );
+
+  return {
+    userEmail: user.email,
+    filename,
+    description,
+    dateAdded,
+    transcript,
+  };
+};
+
+/**
+ * Save a transcript object to the appropriate DAO.
+ *
+ * @async
+ * @function
+ * @param {Object} transcriptObj - An object containing information about a transcript.
+ * @param {string} transcriptObj.userEmail - The email address of the user who created the transcript.
+ * @param {string} transcriptObj.filename - The name of the audio file that was transcribed.
+ * @param {string} transcriptObj.description - A description of the audio file.
+ * @param {string} transcriptObj.dateAdded - The date the audio file was added.
+ * @param {string} transcriptObj.transcript - The text of the transcript.
+ * @param {boolean} enableSpeakerDiarization - Whether to enable speaker diarization.
+ * @returns {Promise<Object>} - A Promise that resolves with the MongoDB model of the saved transcript.
+ */
+const saveTranscript = async (transcriptObj, enableSpeakerDiarization) => {
+  if (enableSpeakerDiarization) {
+    console.log("Calling Diarized Transcript Dao");
+    const diarizedTranscriptDao = new DiarizedTranscriptDao();
+    return await diarizedTranscriptDao.create(transcriptObj);
+  } else {
+    console.log("Calling Transcript Dao");
+    const transcriptDao = new TranscriptDao();
+    return await transcriptDao.create(transcriptObj);
+  }
+};
+
+/**
+ * Handle an AWS Lambda event.
+ *
+ * @async
+ * @function
+ * @param {Object} event - The event object passed by AWS Lambda.
+ * @param {Object} context - The context object passed by AWS Lambda.
+ * @param {function} context.callbackWaitsForEmptyEventLoop - A function that sets whether the event loop should wait for pending events to complete.
+ * @returns {Promise<Object>} - A Promise that resolves with an HTTP response object.
+ */
 const myHandler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   try {
     const user = event.user;
     const body = event.body;
-    const audioFileObj = JSON.parse(body);  
-    let languageCode = "en-US"
-    if (audioFileObj.language === "Spanish") languageCode = "es-ES"
-    const bucketPath = `gs://${bucketName}/${user.email}/${audioFileObj.filename}`
-    await mongooseConnect()
-    const transcript = await transcribeAudioFile(
-      bucketPath, 
-      languageCode,
-      audioFileObj.enableSpeakerDiarization,
-      audioFileObj.minSpeakerCount,
-      audioFileObj.maxSpeakerCount
-    )
-    // const transcriptDao
-    // PUT THESE TWO IN MONGO 
-      // audioFileObj.description,
-      // audioFileObj.dateAdded
-      
-    if (audioFileObj.enableSpeakerDiarization) {
-      // call DiarizedTranscriptDao
-      console.log("Calling Diarized Transcript Dao")
-      const diarizedTranscriptDao = new DiarizedTranscriptDao()
-    } else {
-      console.log("Calling Transcript Dao")
-      const transcriptDao = new TranscriptDao()
-      const mongoTranscriptModel = await transcriptDao.create(transcript)
-      
-    }
-  
+    const audioFileObj = JSON.parse(body);
+
+    const transcriptObj = await createTranscript(audioFileObj, user);
+    const mongoTranscriptModel = await saveTranscript(
+      transcriptObj,
+      audioFileObj.enableSpeakerDiarization
+    );
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ ok: true, status: 200 })
-    }
-  } catch(error) {
+      body: JSON.stringify({
+        ok: true,
+        status: 200,
+        message: "Succesful processing.",
+        payload: mongoTranscriptModel,
+      }),
+    };
+  } catch (error) {
     console.log("error", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ ok: false, error: "Internal server error" }),
+      body: JSON.stringify({
+        ok: false,
+        status: 501,
+        error: "Internal server error",
+      }),
     };
   }
-}
+};
 
-// export handler with middleware
+/**
+ * A middleware-wrapped AWS Lambda handler function that uses a JWT token for authentication.
+ *
+ * @constant
+ * @type {function}
+ * @param {function} myHandler - The AWS Lambda handler function to be wrapped with middleware.
+ * @returns {function} - A middleware-wrapped AWS Lambda handler function that uses a JWT token for authentication.
+ */
 export const handler = middy(myHandler).use(
   verifyTokenMiddleware({ secret: jwtSecret })
 );
